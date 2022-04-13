6c45abb35603932242af474cd3162a8f6abbb465
Nurbolot Boobekov <nurbolot.boobekov.pls@gmail.com>
2022-04-13 14:38:35 +0300

    fixed bug

--- js/index.js
+++ js/index.js
@@ -1,15 +1,24 @@
 const burger = document.getElementsByClassName("header__burger")[0];
 const header = document.getElementsByClassName("header")[0];
+const body = document.getElementsByTagName("body")[0];
 const closeBtn = document.getElementsByClassName("header__close-btn")[0];
 
 burger.addEventListener("click", () => {
-  if (!header.classList.contains("header-active")) {
+  if (
+    !header.classList.contains("header-active") &&
+    !body.classList.contains("body-lock")
+  ) {
     header.classList.add("header-active");
+    body.classList.add("body-lock");
   }
 });
 
 closeBtn.addEventListener("click", () => {
-  if (header.classList.contains("header-active")) {
+  if (
+    header.classList.contains("header-active") &&
+    body.classList.contains("body-lock")
+  ) {
     header.classList.remove("header-active");
+    body.classList.remove("body-lock");
   }
 });
